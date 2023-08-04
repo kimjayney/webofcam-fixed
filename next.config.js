97622989f10a7486d109602b8f5df75b8a451ed8
jayneykim <gim515@naver.com>
2023-08-04 23:55:37 +0900

    csp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const { createSecureHeaders } = require("next-secure-headers");
 
-module.exports ={
+module.exports = {
   async headers() {
     return [
       {
@@ -10,19 +10,22 @@ module.exports ={
             directives: {
               defaultSrc: [
                 "'self'",
-                "http://192.168.0.12",
+                "http://192.168.0.12", // 이 부분은 필요에 따라 수정
+              ],
+              scriptSrc: [
+                "'self'",
+                "'unsafe-inline'", // 필요한 경우에만 사용
+                "https://cdn.example.com", // 허용할 스크립트의 다른 출처
               ],
               styleSrc: ["'self'", "'unsafe-inline'"],
-              imgSrc: ["'self'"],
-              baseUri: "self",
-              formAction: "self",
-              frameAncestors: true,
+              imgSrc: ["'self'", "http://192.168.0.12", "data:"], // 필요한 경우에 따라 수정
+              baseUri: "'self'",
+              formAction: "'self'",
+              frameAncestors: ["'self'", "https://example.com"], // 필요한 경우에 따라 수정
             },
           },
-         
         }),
       },
-      
     ];
   },
 };
